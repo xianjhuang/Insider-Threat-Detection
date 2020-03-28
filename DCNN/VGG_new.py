d51e8f85262c8238f6d618a0f1c740ed6dba24aa
gayathrirg <59992326+gayathrirg@users.noreply.github.com>
2020-03-28 15:22:33 +1100

    Update VGG_new

--- DCNN/VGG_new.py
+++ DCNN/VGG_new.py
@@ -7,42 +7,32 @@ Created on Wed Mar  4 19:08:09 2020
 
 import numpy as np
 import keras
+import keras.metrics
 import matplotlib.pyplot as plt
+
 from keras.layers import Dense,GlobalAveragePooling2D,Dropout,Conv2D,MaxPooling2D,BatchNormalization
 from keras.applications import MobileNetV2,vgg16,vgg19
 from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.optimizers import Adam, RMSprop,SGD
-import keras.metrics
-import tensorflow as tf
-
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
-from sklearn.metrics import cohen_kappa_score
-from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix,classification_report
-from sklearn.metrics import balanced_accuracy_score
 from keras import models
 print(keras.__version__)
 
 image_size = 32
 IMG_SHAPE = (image_size, image_size, 3)
 
-#base_model=keras.applications.mobilenet_v2.MobileNetV2(input_shape=IMG_SHAPE,weights='imagenet',include_top=False,classes=2) #imports the mobilenet model and discards the last 1000 neuron layer.
+#Create the VGG model using Keras
 base_model=vgg19.VGG19(input_shape=IMG_SHAPE,weights='imagenet',include_top=False,classes=2) #imports the mobilenet model and discards the last 1000 neuron layer.
 
-#base_model.trainable=False
-#print(base_model.summary())
-#
-#
 layer_name='block1_pool'
 my_model=Model(inputs=base_model.input,output=base_model.get_layer(layer_name).output)
-print(my_model.summary())
-#
-#
+
 model=models.Sequential()
 model.add(my_model)
 model.add(Conv2D(32,(3,3),activation='relu',padding='same'))
@@ -57,22 +47,15 @@ model.add(Dense(2,activation='softmax'))
 model.layers[0].trainable=False
 model.summary()
 
-#for i,layer in enumerate(base_model.layers):
-#  print(i,layer.name)
-
-#layers = [(layer, layer.name, layer.trainable) for layer in model.layers] pd.DataFrame(layers, columns=['Layer Type', 'Layer Name', 'Layer Trainable'])
-
-#print(layers)
-
 from keras.utils.vis_utils import plot_model
 plot_model(model, to_file='model_vgg_plot.png', show_shapes=True, show_layer_names=True)
 
-# In[5]:
-TRAIN_DIR = 'C:\\Users\\gradhabaigopina\\Downloads\\\CERT_Data\\CERT_Data_Ratio_100'
-PRED_DIR = 'C:\\Users\\gradhabaigopina\\Downloads\\CERT_Data\\Test_Data'
+#Specify the folder that contains the training and test data
+TRAIN_DIR = 'CERT_Data_Ratio_100'
+PRED_DIR = 'Test_Data'
 
-data_gen=ImageDataGenerator(validation_split=0.4,preprocessing_function=preprocess_input) #included in our dependencies
-#
+data_gen=ImageDataGenerator(validation_split=0.4,preprocessing_function=preprocess_input)
+
 train_generator=data_gen.flow_from_directory(TRAIN_DIR,
                                                  target_size=(32,32),
                                                  color_mode='rgb',
@@ -80,7 +63,7 @@ train_generator=data_gen.flow_from_directory(TRAIN_DIR,
                                                  class_mode='categorical',
                                                  shuffle=True, 
                                                  subset='training')
-#
+
 validation_generator = data_gen.flow_from_directory(TRAIN_DIR,
                                                  target_size=(32,32),
                                                  color_mode='rgb',
@@ -100,21 +83,14 @@ test_generator = test_datagen.flow_from_directory(PRED_DIR,
 import collections
 train_Count = train_generator.classes
 
-print("Training.......Number of records.......",collections.Counter(train_Count))
-
 Val_Count = validation_generator.classes
-
-print("Validation....Number of records.........",collections.Counter(Val_Count))
 
 opt = RMSprop(lr=0.0001)
 
 model.compile(optimizer=opt,loss='binary_crossentropy',metrics=['accuracy'])#,keras.metrics.Precision(),keras.metrics.Recall()]) #,tf.keras.metrics.AUC(),tf.keras.metrics.Precision(),tf.keras.metrics.Recall()])
-# Adam optimizer
-# loss function will be categorical cross entropy
-# evaluation metric will be accuracy
+
 step_size_train=train_generator.n//train_generator.batch_size
 
-#Use the fit_generator method of the ImageDataGenerator class to train the network.
 epochs = 15
 
 history = model.fit_generator(generator=train_generator,
@@ -170,9 +146,6 @@ print(classification_report(validation_generator.classes, y_pred, target_names=t
 
 val_trues = validation_generator.classes
 
-#print("val_preds .........  ", val_preds)
-#print("val_trues .......... ",val_trues)
-
 ## accuracy: (tp + tn) / (p + n)
 accuracy = accuracy_score(val_trues, y_pred)
 print('Accuracy: %f' % accuracy)
@@ -185,41 +158,3 @@ print('Recall: %f' % recall)
 ## f1: 2 tp / (2 tp + fp + fn)
 f1 = f1_score(val_trues, y_pred)
 print('F1 score: %f' % f1)
-#
-## kappa
-kappa = cohen_kappa_score(val_trues, y_pred)
-print('Cohens kappa: %f' % kappa)
-## ROC AUC
-##auc = roc_auc_score(val_trues, Y_pred)
-##print('ROC AUC: %f' % auc)
-## confusion matrix
-#matrix = confusion_matrix(val_trues, val_preds)
-#print(matrix)
-#
-tn, fp, fn, tp = confusion_matrix(val_trues, y_pred).ravel()
-print("True negative ....   ",tn)
-print("True positive ....   ",tp)
-
-print("False positive ....   ",fp)
-print("False negative ....   ",fn)
-#
-#print(classification_report(val_trues, val_preds))
-#
-#
-bal_acc = balanced_accuracy_score(val_trues, y_pred)
-print('Balanced Accuracy: %f' % bal_acc)
-#
-from sklearn.metrics import precision_recall_curve
-precision, recall, thresholds = precision_recall_curve(val_trues, y_pred)
-
-plt.plot(recall, precision, label=thresholds)
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.ylim([0.0, 1.05])
-plt.xlim([0.0, 1.0])
-plt.title('Precision-Recall')
-plt.legend(loc="upper right")
-plt.savefig("prec-recall.png")
-plt.show()
-#
-#
